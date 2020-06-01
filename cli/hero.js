a2342a8d4779e2e9195e0c6cdb07029a28d0bd10
victormdev <victoralexandrino99@gmail.com>
2020-05-31 23:44:47 -0300

    CRUD with command line (create)

---
+++ cli/hero.js
@@ -0,0 +1,10 @@
+class Hero {
+    // para extrair somente o necessário para exibição no console
+    constructor({ nome, poder, id }){
+        this.nome = nome,
+        this.poder = poder,
+        this.id = id
+    }
+}
+
+module.exports = Hero
---
+++ cli/index.js
@@ -0,0 +1,32 @@
+// importando o pacote commander
+const Commander = require('commander')
+// importando o database
+const Database = require('./database')
+const Hero = require('./hero')
+async function main(){
+    Commander
+    .version('v1')
+    // atribui uma váriavel para nome, [value] = obtém o valor
+    .option('-n, --nome [value]', "Nome do heroi")
+    // atribui uma variável para poder, [value] = obtém o valor
+    .option('-p, --poder [value]', "Poder do heroi")
+
+    .option('-c, --cadastrar', "Cadastrar um heroi")
+    .parse(process.argv)
+    const hero = new Hero(Commander)
+    try{
+        if(Commander.cadastrar){
+            const resultado = await Database.cadastrar(hero)
+            if(!resultado){
+                console.error('O heroi não foi cadastrado')
+                return
+            }
+            console.log('Heroi cadastrado com sucesso!')
+        }
+    }
+    catch(error){
+        console.error('Operação não finalizada', error)
+    }
+
+}
+main()
